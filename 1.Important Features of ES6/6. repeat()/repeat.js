3ddf00afee27e46d85ad7cfc91c4613d8a78a0db
keystroke99 <keystroke99@gmail.com>
2023-11-30 13:52:48 +0530

    string repeat() method

---
+++ 1.Important Features of ES6/6. repeat()/repeat.js
@@ -0,0 +1,12 @@
+`use strict`
+
+/* 
+    The repeat() method returns a new string with a number of copies of the given string!
+*/
+
+let text = "Wassup Dude!!!"
+let result = text.repeat(2);
+console.log(result);
+
+const mood = 'Happy! ';
+console.log(`I'm very ${mood.repeat(3)}`);
